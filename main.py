c51e3aa9222af9894c991791e13a4174f39b18b0
niki <94849413+nikidersniki@users.noreply.github.com>
2024-09-26 18:45:52 +0200

    Add Theme Support and Scrollinng to Lists

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ import json
 
 eel.init(("web"))
 
+@eel.expose
 def loadOnStartup():
     DocumentFolder = Path.home() / 'Documents'
     configExists = os.path.exists(str(DocumentFolder) + "\GHLM\Locks.json")
     if configExists == True:
         path = str(DocumentFolder) + "\GHLM\Locks.json"
-        print(path)
         f = open(path, "r")
         jsonStr = f.read()
-        jsonA = json.loads(jsonStr)
-        initialize(jsonA["github"])
-        print(jsonA["github"])
+        if (jsonStr == ""):
+            os.remove(str(DocumentFolder) + "\GHLM\Locks.json")
+        else:
+            jsonA = json.loads(jsonStr)
+            print("Loaded Theme: " + str(jsonA["theme"]))
+            eel.loadStyle(jsonA["theme"])
+            if (jsonA["openLast"] == "True"):                 
+                    if (jsonA["github"] != ""):
+                        initialize(jsonA["github"])
     else:
         return 0
 
@@ -49,12 +55,7 @@ def initialize(path):
         parts = origin.split("/")
         reponame = parts[4].strip(".git")
         eel.SetTitle(reponame)
-        locked = subprocess.run(['git', 'lfs', 'locks'], capture_output=True, text=True).stdout.split("\n")
-        for i in locked:
-            if i != "":
-                p = i.split("\t")
-                eel.AddFile(p)
-                print(p)
+        reloadPY()
         DocumentFolder = Path.home() / 'Documents'
         print(str(DocumentFolder) + "\GHLM\Locks.json")
         configExists = os.path.exists(str(DocumentFolder) + "\GHLM\Locks.json")
@@ -65,12 +66,28 @@ def initialize(path):
             dog_data = {
               "github": path,
               "type": "Unreal",
+              "theme": 0,
+              "openLast" : "True"
             }
             dog_data = json.dumps(dog_data)
             print(dog_data)
             f.write(dog_data)
             f.close()
-
+        elif configExists:
+            print("Loaded Config")
+            jsonpath = str(DocumentFolder) + "\GHLM\Locks.json"
+            f = open(jsonpath, "r")
+            jsonStr = f.read()
+            jsonA = json.loads(jsonStr)
+            eel.ToggleSwitches(jsonA["openLast"])
+            if (jsonA["github"] == ""):
+                jsonA["github"] = path
+                data = json.dumps(jsonA)
+                f.close()
+                f = open(jsonpath, "w")
+                f.write(data)
+                f.close()
+         
         eel.ToggleButtons()
     else:
         return 0
@@ -87,10 +104,25 @@ def reloadPY():
     for i in locked:
         if i != "":
             p = i.split("\t")
+            text = p[0]
+            text = text.replace(" ", "")
+            leng = len(text)
+            p.append("")
+            if(leng > 40):
+                actualLength = len(text)
+                actualLength = actualLength * -1 + 40
+                text = text[::-1]
+                text = text[:actualLength]
+                points = "&#x2026;"[::-1]
+                text = text + points
+                text = text[::-1]
+                p[3] = text
+            else:
+                p[3] = p[0]
             eel.AddFile(p)
-            print(p)
 
 def lockFile(filepath):
+    print('git lfs lock' + filepath)
     result = subprocess.run(['git', 'lfs', 'lock', filepath], capture_output=True, text=True).stdout.split("\n")
     eel.reloadJS()
     print(result)
@@ -126,12 +158,52 @@ def Search(name):
         eel.populateSearch(stripped)
 
 @eel.expose
+def changeThemePy(theme):
+    DocumentFolder = Path.home() / 'Documents'
+    path = str(DocumentFolder) + "\GHLM\Locks.json"
+    print(path)
+    f = open(path, "r")
+    jsonStr = f.read()
+    jsonA = json.loads(jsonStr)
+    jsonA["theme"] = theme
+    f.close()
+    f = open(path, "w")
+    modified_json = json.dumps(jsonA)
+    print(modified_json)
+    f.write(modified_json)
+    f.close()
+    eel.loadStyle(theme)
+
+@eel.expose
+def loadTheme():
+    DocumentFolder = Path.home() / 'Documents'
+    path = str(DocumentFolder) + "\GHLM\Locks.json"
+    print(path)
+    f = open(path, "r")
+    jsonStr = f.read()
+    jsonA = json.loads(jsonStr)
+    print("Loaded Theme: " + jsonA["theme"])
+    changeThemePy(jsonA["theme"])
+    f.close()
+
+
+@eel.expose
 def logout():
     DocumentFolder = Path.home() / 'Documents'
-    os.remove(str(DocumentFolder) + "\GHLM\Locks.json")  
+    path = str(DocumentFolder) + "\GHLM\Locks.json"
+    f = open(path, "r")
+    jsonStr = f.read()
+    jsonA = json.loads(jsonStr)
+    jsonA["github"] = ""
+    f = open(path, "w")
+    modified_json = json.dumps(jsonA)
+    print(modified_json)
+    f.write(modified_json)
+    f.close()
+    f.close()
+    #os.remove(str(DocumentFolder) + "\GHLM\Locks.json")  
     eel.reloadApp()
 
-loadOnStartup()
 
 eel.start('index.html', port=8000, size=(800, 610))
 
